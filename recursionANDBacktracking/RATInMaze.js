608db0ecf86167036f7abcf07d68e4bd9342cbb7
jaspal <jaspalchandramola29@gmail.com>
2020-08-04 00:40:34 +0530

    rat in maze problem

--- recursionANDBacktracking/RATInMaze.js
+++ recursionANDBacktracking/RATInMaze.js
@@ -1,12 +1,13 @@
 
+const goalX=2
+const goalY=1
 
-function isValidWord(word) {
-    dict = ["cat", "bat", "to", "binpute"]
-    console.log(dict.includes(word))
-    return dict.includes(word);
+function reached(x,y) {
+   
+    return goalX==x && y==goalY;
 }
 
-let input = [["c", "b", "t"], ["a", "z", "x"], ["t", "m", "n"]]
+let input = [[1, 0, 1], [1, 1, 0], [0, 1, 0]]
 
 
 
@@ -20,7 +21,7 @@ for (let i = 0; i < input.length; i++) {
 }
 
 function isVisited(i, j, track) {
-    if (track[i][j] == 1) {
+    if (track[i][j] == -1) {
         return true;
     }
     return false;
@@ -42,12 +43,12 @@ function AllVisited(track) {
 function isValidBox(x, y, input) {
     let allowedRow = input.length;
     let allowedColumn = input[0].length;
-    return (x < allowedRow && x > -1) && (y < allowedColumn && y > -1);
+    return (x < allowedRow && x > -1) && (y < allowedColumn && y > -1) && input[x][y]==1;
 }
 
-function Boggle(row, column, word, input, track) {
- if (isValidWord(word)) {
-        console.log(word)
+function RatInMaze(row, column,path,input, track) {
+ if (reached(row,column)) {
+        console.log(path)
     }
     rows = [0, 0, 1, -1, 1, -1, -1, 1]
     columns = [1, -1, 0, 0, 1, 1, -1, -1]
@@ -56,19 +57,16 @@ function Boggle(row, column, word, input, track) {
        let newrow = rows[i] + row;
        let newcol = columns[i] + column
         if (isValidBox(newrow, newcol, input) && !isVisited(newrow, newcol, track)) {
-            track[newrow][newcol] = 1;
-            Boggle(newrow, newcol, word + input[newrow][newcol], input, track)
+            track[newrow][newcol] = -1;
+            RatInMaze(newrow, newcol,path+`{${newrow},${newcol} }`, input, track)
             track[newrow][newcol] = 0;
         }
     }
     
 }
 
+track[0][0]=-1
+RatInMaze(0, 0, `{0,0}`, input, track);
 
-for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[i].length; j++) {
-        track[i][j] = 1
-        Boggle(0, 0, input[i][j], input, track);
-        track[i][j] = 0
-    }
-}
+
+
